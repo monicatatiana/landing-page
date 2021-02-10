fc978bf1ef930aa370e6c2253fe9406153191ebf
monicatatiana <monictatiana@gmail.com>
2021-02-10 05:20:26 +0000

    landing page

---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+	return (
+		<nav className="navbar fixed-bottom navbar-light bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					Fixed bottom
+				</a>
+			</div>
+		</nav>
+	);
+};
